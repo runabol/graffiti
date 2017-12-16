e3af944d04c5f685f15e223a681929b2e5fae536
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2017-12-16 13:16:06 -0500

    adding nodes.

--- src/test/java/com/creactiviti/graffiti/SqlGraphTests.java
+++ src/test/java/com/creactiviti/graffiti/SqlGraphTests.java
@@ -1,11 +1,13 @@
 package com.creactiviti.graffiti;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.creactiviti.graffiti.graph.Node;
 import com.creactiviti.graffiti.graph.sql.SqlGraph;
 import com.creactiviti.graffiti.graph.sql.SqlNode;
 
@@ -16,8 +18,16 @@ public class SqlGraphTests {
   @Autowired private SqlGraph g;
 
 	@Test
-	public void testAdd1() {
-	  g.add(SqlNode.builder(g).type("Movie").property("title", "Home Alone").build());
+	public void testAddNode () {
+	  Node node = SqlNode.builder(g)
+      	                .type("Movie")
+      	                .property("title", "Home Alone")
+      	                .build();
+    Node added = g.add(node);
+    Assert.assertNotNull(added.id());
+    Node get = g.nodes().hasId(added.id()).next();
+    Assert.assertEquals(get.id(), added.id());
 	}
+	
 
 }
